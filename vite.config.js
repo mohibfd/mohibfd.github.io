b3f085e75c9c608785d1eacfdbaf59a60c12f514
mohibfd <mohib.akoum@hotmail.com>
2024-04-28 17:55:10 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import glsl from "vite-plugin-glsl";
-import { defineConfig } from "vite";
+const isCodeSandbox = !!process.env.SANDBOX_URL
 
-export default defineConfig({
-   plugins: [glsl()],
-});
+export default {
+    root: "src/",
+    publicDir: "../static/",
+    base: "./",
+    server:
+    {
+        host: true,
+        open: !isCodeSandbox // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: "../dist",
+        emptyOutDir: true,
+        sourcemap: true
+    }
+}
