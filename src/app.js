28f017e1188284e5679530c1e1283973ff3c681a
mohibfd <mohib-akoum@hotmail.com>
2024-04-28 17:29:14 +0100

    testing with code commented out

--- src/app.js
+++ src/app.js
@@ -80,139 +80,141 @@ window.atBottom = false;
 var ring = new Ring(scene, renderer);
 ring.generate(mohibPositions);
 
-// Trippy ball
-let trippyBall = new TrippyBall(scene);
-trippyBall.generate(mohibPositions);
+// // Trippy ball
+// let trippyBall = new TrippyBall(scene);
+// trippyBall.generate(mohibPositions);
 
-// Wormhole
-var wormhole = new Wormhole(scene, library, parameters);
-wormhole.generate();
-wormhole.active();
+// // Wormhole
+// var wormhole = new Wormhole(scene, library, parameters);
+// wormhole.generate();
+// wormhole.active();
 
-// Singularity
-var blackhole = new Singularity(scene, library, parameters);
-var pos = new THREE.Vector3(
-   wormhole.initialWormholePosition.x + wormhole.xShift + 3,
-   wormhole.initialWormholePosition.y + wormhole.yShift + 3,
-   wormhole.initialWormholePosition.z
-);
-blackhole.generate(pos);
-blackhole.show();
+// // Singularity
+// var blackhole = new Singularity(scene, library, parameters);
+// var pos = new THREE.Vector3(
+//    wormhole.initialWormholePosition.x + wormhole.xShift + 3,
+//    wormhole.initialWormholePosition.y + wormhole.yShift + 3,
+//    wormhole.initialWormholePosition.z
+// );
+// blackhole.generate(pos);
+// blackhole.show();
 
-// Scroll Animation
-var scrollAnimation = new ScrollAnimation(moon, mohib, camera);
-document.body.onscroll = scrollAnimation.animate;
+// // Scroll Animation
+// var scrollAnimation = new ScrollAnimation(moon, mohib, camera);
+// document.body.onscroll = scrollAnimation.animate;
 
-// Animation Loop
-let hyperlinkClicked = false;
+// // Animation Loop
+// let hyperlinkClicked = false;
 
-var el1_code = document.getElementById("link1_source");
-var el2_code = document.getElementById("link2_source");
-var el3_code = document.getElementById("link3_source");
-var el4 = document.getElementById("link4");
-var el5 = document.getElementById("link5");
+// var el1_code = document.getElementById("link1_source");
+// var el2_code = document.getElementById("link2_source");
+// var el3_code = document.getElementById("link3_source");
+// var el4 = document.getElementById("link4");
+// var el5 = document.getElementById("link5");
 
-el1_code.onclick = setHyperlink1CodeTrue;
-el2_code.onclick = setHyperlink2CodeTrue;
-el3_code.onclick = setHyperlink5CodeTrue;
-el4.onclick = setHyperlink6True;
-el5.onclick = setHyperlink7True;
+// el1_code.onclick = setHyperlink1CodeTrue;
+// el2_code.onclick = setHyperlink2CodeTrue;
+// el3_code.onclick = setHyperlink5CodeTrue;
+// el4.onclick = setHyperlink6True;
+// el5.onclick = setHyperlink7True;
 
-window.allowCamChange = true;
-var firstLinkClicked = true;
-function hyperlinkClickedFunc() {
-   if (firstLinkClicked) {
-      allowCamChange = false;
-      hyperlinkClicked = true;
+// window.allowCamChange = true;
+// var firstLinkClicked = true;
+// function hyperlinkClickedFunc() {
+//    if (firstLinkClicked) {
+//       allowCamChange = false;
+//       hyperlinkClicked = true;
 
-      wormhole.animate();
-      wormhole.playSound(camera);
-      firstLinkClicked = false;
-   }
-}
+//       wormhole.animate();
+//       wormhole.playSound(camera);
+//       firstLinkClicked = false;
+//    }
+// }
 
-var href = "";
-window.redirect = false;
+// var href = "";
+// window.redirect = false;
 
-function setHyperlink1True() {
-   href = "https://explain-sensitivity-classifier.herokuapp.com/";
-   hyperlinkClickedFunc();
-}
-function setHyperlink1CodeTrue() {
-   href = "https://github.com/mohibfd/Sensitivity-Classification";
-   hyperlinkClickedFunc();
-}
-function setHyperlink2True() {
-   href =
-      "https://drive.google.com/file/d/1j8yN8HW-xrE2MfBpt8LRP4-efW08d7KG/view?usp=sharing";
-   hyperlinkClickedFunc();
-}
-function setHyperlink2CodeTrue() {
-   href = "https://github.com/mohibfd/MyApp";
-   hyperlinkClickedFunc();
-}
-function setHyperlink3True() {
-   href =
-      "https://colab.research.google.com/drive/1SfVwCDeRtU1I8rbjl-FOBSEwtAp0SEYD?usp=sharing";
-   hyperlinkClickedFunc();
-}
-function setHyperlink3CodeTrue() {
-   href =
-      "https://colab.research.google.com/drive/1SfVwCDeRtU1I8rbjl-FOBSEwtAp0SEYD?usp=sharing";
-   hyperlinkClickedFunc();
-}
-function setHyperlink4True() {
-   href =
-      "https://colab.research.google.com/drive/1SRjiZbdBZL9Ny6akgLhu_AMPGGwcaOlZ?usp=sharing";
-   hyperlinkClickedFunc();
-}
-function setHyperlink4CodeTrue() {
-   href =
-      "https://colab.research.google.com/drive/1SRjiZbdBZL9Ny6akgLhu_AMPGGwcaOlZ?usp=sharing";
-   hyperlinkClickedFunc();
-}
-function setHyperlink5True() {
-   href =
-      "https://drive.google.com/file/d/1jshTnbdfVAQLa-TkQAK9w6fUNKgzDu6i/view?usp=sharing";
-   hyperlinkClickedFunc();
-}
-function setHyperlink5CodeTrue() {
-   href = "https://github.com/mohibfd/Doctor-Project";
-   hyperlinkClickedFunc();
-}
-function setHyperlink6True() {
-   href = "https://github.com/mohibfd";
-   hyperlinkClickedFunc();
-}
-function setHyperlink7True() {
-   href = "https://www.linkedin.com/in/mohib-akoum";
-   hyperlinkClickedFunc();
-}
+// function setHyperlink1True() {
+//    href = "https://explain-sensitivity-classifier.herokuapp.com/";
+//    hyperlinkClickedFunc();
+// }
+// function setHyperlink1CodeTrue() {
+//    href = "https://github.com/mohibfd/Sensitivity-Classification";
+//    hyperlinkClickedFunc();
+// }
+// function setHyperlink2True() {
+//    href =
+//       "https://drive.google.com/file/d/1j8yN8HW-xrE2MfBpt8LRP4-efW08d7KG/view?usp=sharing";
+//    hyperlinkClickedFunc();
+// }
+// function setHyperlink2CodeTrue() {
+//    href = "https://github.com/mohibfd/MyApp";
+//    hyperlinkClickedFunc();
+// }
+// function setHyperlink3True() {
+//    href =
+//       "https://colab.research.google.com/drive/1SfVwCDeRtU1I8rbjl-FOBSEwtAp0SEYD?usp=sharing";
+//    hyperlinkClickedFunc();
+// }
+// function setHyperlink3CodeTrue() {
+//    href =
+//       "https://colab.research.google.com/drive/1SfVwCDeRtU1I8rbjl-FOBSEwtAp0SEYD?usp=sharing";
+//    hyperlinkClickedFunc();
+// }
+// function setHyperlink4True() {
+//    href =
+//       "https://colab.research.google.com/drive/1SRjiZbdBZL9Ny6akgLhu_AMPGGwcaOlZ?usp=sharing";
+//    hyperlinkClickedFunc();
+// }
+// function setHyperlink4CodeTrue() {
+//    href =
+//       "https://colab.research.google.com/drive/1SRjiZbdBZL9Ny6akgLhu_AMPGGwcaOlZ?usp=sharing";
+//    hyperlinkClickedFunc();
+// }
+// function setHyperlink5True() {
+//    href =
+//       "https://drive.google.com/file/d/1jshTnbdfVAQLa-TkQAK9w6fUNKgzDu6i/view?usp=sharing";
+//    hyperlinkClickedFunc();
+// }
+// function setHyperlink5CodeTrue() {
+//    href = "https://github.com/mohibfd/Doctor-Project";
+//    hyperlinkClickedFunc();
+// }
+// function setHyperlink6True() {
+//    href = "https://github.com/mohibfd";
+//    hyperlinkClickedFunc();
+// }
+// function setHyperlink7True() {
+//    href = "https://www.linkedin.com/in/mohib-akoum";
+//    hyperlinkClickedFunc();
+// }
 
-var clock = new THREE.Clock();
+// var clock = new THREE.Clock();
 
-function animate() {
-   requestAnimationFrame(animate);
+// function animate() {
+//    requestAnimationFrame(animate);
 
-   moon.rotation.x += 0.005;
+//    moon.rotation.x += 0.005;
 
-   ring.animate();
+//    ring.animate();
 
-   trippyBall.animate();
+//    trippyBall.animate();
 
-   if (hyperlinkClicked == true) {
-      wormhole.wormholeAnimation(camera);
-      if (window.redirect == true) {
-         window.open(href);
-         window.location.reload();
-      }
-   }
+//    if (hyperlinkClicked == true) {
+//       wormhole.wormholeAnimation(camera);
+//       if (window.redirect == true) {
+//          window.open(href);
+//          window.location.reload();
+//       }
+//    }
 
-   window.materialToUpdate.uniforms.uTime.value = clock.getElapsedTime();
-   window.meshToUpdate.rotateZ(2);
-   window.mesh2ToUpdate.rotateY(2);
+//    window.materialToUpdate.uniforms.uTime.value = clock.getElapsedTime();
+//    window.meshToUpdate.rotateZ(2);
+//    window.mesh2ToUpdate.rotateY(2);
 
-   renderer.render(scene, camera);
-}
+//    renderer.render(scene, camera);
+// }
 
-animate();
+// animate();
+
+renderer.render(scene, camera);
